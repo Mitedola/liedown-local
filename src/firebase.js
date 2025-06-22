358bf0c91c1b1382c7f675caaa5c81f58a32308d
Will Crawford Jr <willcrawfordjr@Will-Crawfords-Macbook-Pro-5.local>
2025-06-22 14:17:33 -0400

    Update Firebase config with real values

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,27 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyChf2YtiXb7nD0n4Iiaw481dBnsvRpiE64",
+  authDomain: "lie-down-local.firebaseapp.com",
+  projectId: "lie-down-local",
+  storageBucket: "lie-down-local.firebasestorage.app",
+  messagingSenderId: "987672322103",
+  appId: "1:987672322103:web:b220681499b012d8951fb2",
+  measurementId: "G-XSN4WTEC1W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
   authDomain: 'YOUR_AUTH_DOMAIN',
   projectId: 'YOUR_PROJECT_ID',
